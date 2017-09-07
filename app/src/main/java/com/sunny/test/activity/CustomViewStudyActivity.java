75efda9e4cabeb35be0164ce5800579bfc3cfd90
Sunny <1250422644@qq.com>
2017-09-07 23:47:47 +0800

    下拉刷新布局

--- app/src/main/java/com/sunny/test/activity/CustomViewStudyActivity.java
+++ app/src/main/java/com/sunny/test/activity/CustomViewStudyActivity.java
@@ -2,14 +2,33 @@ package com.sunny.test.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.sunny.test.R;
+import com.sunny.test.adapter.EasyStringAdapter;
+import com.sunny.test.views.EasyRefreshLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class CustomViewStudyActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.refreshLayout)
+    EasyRefreshLayout refreshLayout;
+
+    private EasyStringAdapter adapter;
+    private List<String> datas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,32 @@ public class CustomViewStudyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_view_study);
         ButterKnife.bind(this);
 
+        initView();
+    }
+
+    private void initView() {
+        for (int i = 0; i < 30; i++) {
+            datas.add("item" + i);
+        }
+        adapter = new EasyStringAdapter(datas);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        adapter.bindToRecyclerView(recyclerView);
+
+        refreshLayout.setOnRefreshListener(() -> {
+            Observable.create(e -> {
+                for (int i = 0; i < 3; i++) {
+                    datas.add(0, "add item" + i);
+                    Thread.sleep(800);
+                }
+                e.onNext(new byte[0]);
+            }).subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(o -> {
+                        adapter.notifyDataSetChanged();
+                        refreshLayout.closeRefresh();
+                    });
+        });
     }
 
 }
--- app/src/main/java/com/sunny/test/views/EasyRefreshLayout.java
+++ app/src/main/java/com/sunny/test/views/EasyRefreshLayout.java
@@ -9,7 +9,10 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.OverScroller;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.sunny.test.R;
 
@@ -45,13 +48,26 @@ public class EasyRefreshLayout extends ViewGroup {
      */
     private float mYLastMove;
 
+    /**
+     * 移动比率，比率越高，下拉相对距离越小，如果等于1，则手指下拉多少距离，就实际下拉多少距离
+     */
+    private float MOVE_RATE = 1.5f;
+
+    /**
+     * 箭头转向比例，如果等于1则表示要头布局完全下拉箭头才转向
+     */
+    private float changeRate = 0.8f;
 
     @LayoutRes
     private int headerResourceId = R.layout.easy_refresh_header;
 
     private View header;
     private View contentView;
+    private ImageView refreshArrow;
+    private TextView refreshText;
+    private ProgressBar refreshProgressBar;
     private LayoutInflater inflater;
+    private OnRefreshListener listener;
 
     /**
      * 是否允许下拉
@@ -69,7 +85,7 @@ public class EasyRefreshLayout extends ViewGroup {
     private Status status = Status.NORMAL;
 
     enum Status {
-        NORMAL, SCROLLING
+        NORMAL, SCROLL_DOWN, SCROLL_DOWN_EFFECT, REFRESHING
     }
 
     public EasyRefreshLayout(Context context) {
@@ -117,6 +133,9 @@ public class EasyRefreshLayout extends ViewGroup {
         //将头布局加载到当前布局
         inflater.inflate(headerResourceId, this, true);
         header = getChildAt(getChildCount() - 1);
+        refreshArrow = (ImageView) header.findViewById(R.id.refresh_arrow);
+        refreshText = (TextView) header.findViewById(R.id.refresh_text);
+        refreshProgressBar = (ProgressBar) header.findViewById(R.id.refresh_progressBar);
     }
 
     @Override
@@ -133,12 +152,14 @@ public class EasyRefreshLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        System.out.println("onLayout");
-        //将头部署在当前视图之外
-        header.layout(0, -header.getMeasuredHeight(), getWidth(), header.getHeight());
-        //部署内容
-        contentView.layout(0, 0, contentView.getMeasuredWidth(), contentView.getMeasuredHeight());
-        headerHeight = header.getMeasuredHeight();
+        if (changed && contentView != null) {
+            System.out.println("onLayout");
+            //将头部署在当前视图之外
+            header.layout(0, -header.getMeasuredHeight(), getWidth(), header.getHeight());
+            //部署内容
+            contentView.layout(0, 0, contentView.getMeasuredWidth(), contentView.getMeasuredHeight());
+            headerHeight = header.getMeasuredHeight();
+        }
     }
 
     @Override
@@ -146,24 +167,74 @@ public class EasyRefreshLayout extends ViewGroup {
         return super.performClick();
     }
 
+    /**
+     * 如果返回true，则会拦截下触摸事件，不向下传递，由本ViewGroup消费
+     * 如果返回false或者执行super，则不拦截，向下传递
+     *
+     * @param ev
+     * @return
+     */
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mYDown = ev.getRawY();
+                mYLastMove = mYDown;
+                updateViewContent();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mYMove = ev.getRawY();
+                float diff = Math.abs(mYMove - mYDown);
+                mYLastMove = mYMove;
+                //当手指拖动值大于TouchSlop值时，认为应该进行滚动，拦截子控件的事件
+                //这样就不会响应子控件的点击或者长按事件
+                if (isMoveAble() && diff > mTouchSlop) {
+                    status = Status.SCROLL_DOWN;
+                    return true;
+                }
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mYDown = event.getRawY();
-                mYLastMove = mYDown;
-                break;
             case MotionEvent.ACTION_MOVE:
+                System.out.println("ACTION_MOVE：" + status);
                 mYMove = event.getRawY();
+                //自按下滚动的距离
+                int scrollDistance = Math.abs(getScrollY());
                 int deltaY = (int) (mYLastMove - mYMove);
-                scrollBy(0, deltaY);
+
+                //根据下滑的比例，改变显示的视图
+                if (scrollDistance > (headerHeight * changeRate)) {
+                    status = Status.SCROLL_DOWN_EFFECT;
+                    updateViewContent();
+                } else {
+                    status = Status.SCROLL_DOWN;
+                    updateViewContent();
+                }
+
+                //如果当前布局向下拉出头布局，并且头布局向下滚动的距离小于等于头布局的高度
+                //或者当前头布局已经拉出，但是正在回滚
+                //两种情况下都执行滚动操作
+                if ((getScrollY() <= 0 && getScrollY() >= -headerHeight)
+                        || (getScrollY() <= 0 && deltaY > 0)) {
+                    scrollBy(0, (int) (deltaY / MOVE_RATE));
+                }
                 mYLastMove = mYMove;
                 break;
             case MotionEvent.ACTION_UP:
+                System.out.println("ACTION_UP");
                 System.out.println("headerHeight: " + headerHeight);
-                int dy = - getScrollY();
-                mScroller.startScroll(0, getScrollY(), 0, dy);
-                invalidate();
+                if (status == Status.SCROLL_DOWN_EFFECT) {
+                    showRefresh();
+                } else {
+                    closeRefresh();
+                }
+                updateViewContent();
                 //如果当前处于正常状态，则相应点击事件
                 if (status == Status.NORMAL)
                     performClick();
@@ -172,44 +243,60 @@ public class EasyRefreshLayout extends ViewGroup {
         return true;
     }
 
+    private void showRefresh() {
+        int dy = -headerHeight - getScrollY();
+        status = Status.REFRESHING;
+        mScroller.startScroll(0, getScrollY(), 0, dy);
+        invalidate();
+        if(listener != null)
+            listener.onRefresh();
+    }
+
+    private void updateViewContent() {
+        switch (status) {
+            case NORMAL:
+            case SCROLL_DOWN:
+                refreshText.setText("下拉刷新");
+                refreshArrow.setRotation(0f);
+                refreshText.setVisibility(VISIBLE);
+                refreshProgressBar.setVisibility(INVISIBLE);
+                refreshArrow.setVisibility(VISIBLE);
+                break;
+            case REFRESHING:
+                refreshText.setText("正在刷新");
+                refreshText.setVisibility(INVISIBLE);
+                refreshProgressBar.setVisibility(VISIBLE);
+                refreshArrow.setVisibility(INVISIBLE);
+                break;
+            case SCROLL_DOWN_EFFECT:
+                refreshText.setText("释放刷新");
+                refreshArrow.setRotation(180f);
+                refreshText.setVisibility(VISIBLE);
+                refreshProgressBar.setVisibility(INVISIBLE);
+                refreshArrow.setVisibility(VISIBLE);
+                break;
+        }
+    }
+
     @Override
     public void computeScroll() {
         //如果还在滚动过程中
         if (mScroller.computeScrollOffset()) {
-            System.out.println("scroll to：" + mScroller.getCurrX() + ", " + mScroller.getCurrY());
             scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
             invalidate();
         }
     }
 
-    /**
-     * 如果返回true，则会拦截下触摸事件，不向下传递，由本ViewGroup消费
-     *
-     * @param ev
-     * @return
-     */
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        switch (ev.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                mYDown = ev.getRawX();
-                mYLastMove = mYDown;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                mYMove = ev.getRawX();
-                float diff = Math.abs(mYMove - mYDown);
-                mYLastMove = mYMove;
-                //当手指拖动值大于TouchSlop值时，认为应该进行滚动，拦截子控件的事件
-                //这样就不会响应子控件的点击或者长按事件
-                if(diff > mTouchSlop){
-                    return true;
-                }
-                break;
+    private boolean isMoveAble() {
+        if (contentView == null)
+            return false;
+        //如果当前在顶部，而且是下滑动作
+        if (isChildScrollToTop() && (mYMove - mYDown) > 0) {
+            return true;
         }
-        return enable && isChildScrollToTop();
+        return false;
     }
-
 
     /**
      * 判断contentView是否已经滑到顶部
@@ -219,5 +306,27 @@ public class EasyRefreshLayout extends ViewGroup {
     private boolean isChildScrollToTop() {
         //如果contentView无法再继续向上滑则表示已经滑到顶部
         return !ViewCompat.canScrollVertically(contentView, -1);
+    }
+
+
+
+
+    public interface OnRefreshListener{
+        void onRefresh();
+    }
+
+    //////////////////////////////////////////////////////////////////
+    ///////  以下是对外的接口
+    //////////////////////////////////////////////////////////////////
+
+    public void setOnRefreshListener(OnRefreshListener listener) {
+        this.listener = listener;
+    }
+
+    public void closeRefresh(){
+        int dy = -getScrollY();
+        status = Status.NORMAL;
+        mScroller.startScroll(0, getScrollY(), 0, dy);
+        invalidate();
     }
 }
